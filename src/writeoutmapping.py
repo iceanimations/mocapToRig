de43fb76634e55f42355cb88135922d0ece128d9
Talha Ahmed <talha.ahmed@gmail.com>
2021-09-20 16:25:04 +0500

    Adding fingers to mixamo

---
+++ src/writeoutmapping.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pymel.core as pc
+from collections import OrderedDict
+import mocapToRig
+
+defnode = pc.PyNode('MocapCharacter1')
+map_hik = OrderedDict([(pc.mel.GetHIKNodeName(x), x) for x in range(212)])
+map_sk = OrderedDict()
+
+for name, num in map_hik.items():
+    jnt = defnode.attr(name).inputs(type='joint')
+    if jnt:
+        map_sk[jnt[0].name().split(':')[-1]] = num
+
+print map_sk 
+
+moctor_base = op.dirname(mocapToRig.__file__)
+sk_path = op.join(moctor_base, 'mappings', 'mixamo.sk.json')
+with open(sk_path, 'w') as _file:
+    json.dump(map_sk, _file, indent=2)
