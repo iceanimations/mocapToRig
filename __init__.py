695f2960d6a3845e81c7955ba586706802a2aa32
Talha Ahmed <talha.ahmed@gmail.com>
2025-06-19 15:06:04 +0300

    change internal imports to relative imports

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-import src._mocapToRig as mr
-import src.moctor as moctor
+import .src._mocapToRig as mr
+import .src.moctor as moctor
 
 reload(mr)
 reload(moctor)
 
-from src._mocapToRig import *
+from .src._mocapToRig import *
